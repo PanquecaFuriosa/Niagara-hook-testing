c367013ad7b93375e319bfc043c2201911d03ac0
xdgorgola <pedro18fe@gmail.com>
2024-01-24 22:42:28 -0400

    Modified test file for action test

--- __tests__/fail.test.tsx
+++ __tests__/fail.test.tsx
@@ -2,6 +2,6 @@ import {describe, test} from '@jest/globals';
 
 describe('Failure test', () => {
     test('Just fails the test', () => {
-        fail("This test just fails!");
+        fail("This test just fails");
     });
 });
