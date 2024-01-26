141e026701f39f6759f22330c6da07abbba2fe3f
PanquecaFuriosa <18-10761@usb.ve>
2024-01-26 10:56:01 -0400

    testing pre-commit hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const holi;
 
 export const metadata: Metadata = {
   title: "Create Next App",
